cfcb065ee4640f2936bba2b34f0d150a0ab2688a
eodnhoj <michael@modernliteracy.org>
2014-10-12 14:34:09 -0400

    Scanner ast.mli successfuly lex'ed.

--- src/ast.mli
+++ src/ast.mli
@@ -1,31 +1,13 @@
 { open Parser }
 
 (*Building Blocks*)
-let sep = ['-' '_'];;
-let lAlpha = ['a' - 'z'];;
-let uAlpha = ['A' - 'Z'];;
-let Alpha = ['a' - 'z' 'A' - 'Z'];;
-let ints = ['0' - '9'];;
-let floats = ['0' - '9']+.['0'-'9'](1,8);; (*TODO: Make sure this regular expression works.*)
-let AlphaNum = ['a' - 'z' 'A' - 'Z' '0' -'9'];;
-let AlphaNumSep = 'a' - 'z' 'A' - 'Z' '0' - '9' '_' '-'];;
-
-(*Only three primitives*)
-let False = 'false';;
-let True = 'true';;
-let bool = (False | True);;
-
-let string = '"'[.]*'"';;
-
-let number = ints*(.nums*(1,8))?;; (*TODO: Confirm this works*)
-
-(*Data types*)
-
-(*Fundamentals*)
-let ids = lAlpha+AlphaNumSep*
-let varNames = lAlpha+AlphaNumSep*;; (*TODO: See if we can compose from previous regex*)
-let funcNames = 
-
+let sep = ['-' '_']
+let lAlpha = ['a' - 'z']
+let uAlpha = ['A' - 'Z']
+let Alpha = ['a' - 'z' 'A' - 'Z']
+let ints = ['0' - '9']
+let AlphaNum = ['a' - 'z' 'A' - 'Z' '0' -'9']
+let AlphaNumSep = ['a' - 'z' 'A' - 'Z' '0' - '9' '_' '-']
 
 rule token =
     parse [' ' '\t' '\r' '\n'] {token lexbuf}
@@ -33,13 +15,13 @@ rule token =
     | '-'			            {MINUS}
     | '*'		      	        {TIMES}
     | '/'		      	        {DIVIDE}
-    | '**' 		      	        {EXPONENT}
-    | ids as id                 {ID(id)} (*TODO: Create a symbol table and link*)
-    | bool as lit               { BOOL(lit) } 
-    | string                    { STRING(str) }
-    | ints+ as lit              {INTEGER(lit)}
-    | floats as lit             {FLOAT(lit)}
-    | (' number '|' String '|' boolean ') {TYPES}
+    | "**" 		      	        {EXPONENT}
+    | Alpha AlphaNumSep* as id                 {ID(id)} (*TODO: Create a symbol table and link*)
+    | ("false" | "true") as lit { BOOL(lit) } 
+    | '"' _* '"'                    { STRING(str) }
+    | ints+ as lit              {INTEGER(int_of_str lit)}
+    | ints* ('.' ints+)? as lit             {FLOAT(float_of_str lit)}
+    | ("number" | "String" | "boolean"  | "void" ) {TYPES}
     | ';'                       {SEQUENCE}
     | '{'     	       	      	{OPENCB}
     | '}'			            {CLOSECB}
@@ -47,8 +29,18 @@ rule token =
     | ')'			            {CLOSEP}
     | '['                       {OPENB}
     | ']'                       {CLOSEB}
-    | 'if'  	       	      	{IF}
-    | 'else'		      	    {ELSE}
-    | 'while'			        {WHILE}
-    | 'for'			            {FOR}
+    | "/*"                      {COMMENTO}
+    | "*/"                      {COMMMENTC}
+    | '>'			            {GT}
+    | '<'			            {LT}
+    | ">="			            {GEQ}
+    | "<="			            {LEQ}
+    | "if"  	       	      	{IF}
+    | "else"		      	    {ELSE}
+    | "while"			        {WHILE}
+    | "for"			            {FOR}
+    | "print"                   {PRINT}
+    | "return"                  {RETURN}
+    | '='                       {ASSIGN}
+    | "=="                      {COMPARE}
     | eof	    	            {EOF}
