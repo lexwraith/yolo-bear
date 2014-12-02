562bbf18f73c6e35ca019db908338adf567891ac
eodnhoj <michael@modernliteracy.org>
2014-12-02 02:25:15 -0500

    Work log and cleanup

---
+++ mikec/semantic.ml
@@ -0,0 +1,6 @@
+(* Takes in ast.program (see definition) and raises error if something doesn't add up*)
+
+(* TODO: Parameter checking
+ *       Type checking
+ *      ???*)
+let check (globals,functions) = function
