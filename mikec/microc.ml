dc370bb96d90e53bc7a2628a78136b5b4ff99b0e
Michael Nguyen <michael@modernliteracy.org>
2014-12-10 14:24:57 -0500

    Test clean/reordering, added test suite

--- mikec/microc.ml
+++ mikec/microc.ml
@@ -3,7 +3,9 @@ type action = Raw | Ast | Compile (*| Semantic*)
 let _ =
   let action = if Array.length Sys.argv > 1 then
     List.assoc Sys.argv.(1) [ ("-r", Raw);
-                              ("-a", Ast);(*("-s", Semantic);*)("-C", Compile)]
+                              ("-a", Ast);
+			    (*("-s", Semantic);*)
+			      ("-C", Compile)]
   else Compile in
   let lexbuf = Lexing.from_channel stdin in
   let program = Parser.program Scanner.token lexbuf in
