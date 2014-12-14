6da280331c0e83d1a4e1e790f724e40d7bdf6a7f
Michael Nguyen <michael@modernliteracy.org>
2014-12-13 21:18:32 -0500

    Yuchen merge

--- mikec/types.ml
+++ mikec/types.ml
@@ -4,7 +4,7 @@ type t =
 	| Float
 	| Char
 	| String
-	| None
+	| Nonetype
 	| Struct of string * ((string * t) array) (* name, fields *)
 	| Array of t * int list										(* type, size *)
 	| DArray of t * int 											(* type, dimension *)                     
@@ -25,7 +25,7 @@ let rec string_of_type t1 =
 		in 
 		(string_of_type t_a) ^ ind	              
 	| Exception(s) -> "Exception: " ^ s
-	| None
+	| Nonetype
 	| _ -> "Unknown type"
 
 let rec output_of_type t1 =
@@ -39,7 +39,7 @@ let rec output_of_type t1 =
 	| Array(t_a, _) ->  output_of_type t_a	  
 	| DArray(t_a, _) -> output_of_type t_a            
 	| Exception(_) -> "exception"
-	| None -> ""
+	| Nonetype -> ""
 	| _ -> ""
 
 let type_from_string s1 =
@@ -49,4 +49,4 @@ let type_from_string s1 =
 	| "char" -> Char
 	| "String" -> String
 	| "float" -> Float
-	| _ -> None
+	| _ -> Nonetype
