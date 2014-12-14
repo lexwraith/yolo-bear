a216437f6ce1ee1213ae2aee489c04fe3786c9d3
ebshanon <ebshanon@gmail.com>
2014-12-14 15:55:52 -0500

    passingarray example now uses pointers

--- mikec/arrays/passingarrays.c
+++ mikec/arrays/passingarrays.c
@@ -4,26 +4,28 @@ int main() {
 
   Array a;
   initArray(&a);
+  Array *ptra = &a;
   int x;  
   for(x=0; x<10; x++){
-    //printf("Test");
-    insertInt(&a, x, x);
-  //  printf("%d\n", a.array[x].i);
+    insertInt(ptra, x, x);
   }
 
-  Array doubleArray(Array funcArray){
+  Array *doubleArray(Array *funcArray){
     for(x=0; x < a.size; x++){
-      insertInt(&a, x, a.array[x].i);
+      insertInt(funcArray, x, 2*(funcArray->array[x].i));
     }
   return funcArray;
   }
   
-  Array b;  
-  b = doubleArray(a);
+  Array b; 
+  initArray(&b); 
+  Array *ptrb = &b;
+
+  ptrb = doubleArray(ptra);
  
   for(x=0; x<10; x++){
-    printf("a: %d\n", a.array[x].i);
-    printf("b: %d\n", b.array[x].i);
+    printf("a: %d\n", ptra->array[x].i);
+    printf("b: %d\n", ptrb->array[x].i);
   }
   
 }
